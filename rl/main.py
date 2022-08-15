a75bfc4431f3ca58c8dafc522fc0634464b88385
simeoncapy <simeoncapy@gmail.com>
2022-08-16 00:01:26 +0900

    update dim action motor

--- rl/main.py
+++ rl/main.py
@@ -4,6 +4,6 @@ import numpy as np
 from QLearning import *
 
 dimStateMotor = len(cst.EMOTION) * cst.DIM_PAD * cst.INTENTION_DIM
-dimActionMotor = len(cst.PALETTE) * len(cst.ACTIONS)
+dimActionMotor = pow(len(cst.ACTIONS), cst.NUMBER_OF_MOTOR)
 
 rl_motor = QLearning(dimStateMotor, dimActionMotor)
