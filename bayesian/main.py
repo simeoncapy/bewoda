b0acf703324ee09ea4c3e4f829222e8c80841784
simeoncapy <simeoncapy@gmail.com>
2022-10-15 02:21:23 +0900

    temp

--- bayesian/main.py
+++ bayesian/main.py
@@ -1,7 +1,12 @@
 import pyAgrum as gum
+import pyAgrum.lib.notebook as gnb
+import pyAgrum.lib.dynamicBN as gdyn
 
 emotionDbn = gum.BayesNet("FutureEmotion")
 positionDbn = gum.BayesNet("FuturePosition")
+
+twodbn=gum.fastBN("d0[3]->ct<-at<-a0->b0->bt<-a0->dt[3]<-d0<-c0->ct;c0->at",6)
+twodbn
 
 
 # https://www.bayesserver.com/docs/introduction/dynamic-bayesian-networks/
